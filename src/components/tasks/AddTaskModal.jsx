312332ea3e5009b5e7140f65237b4035c4f80a34
yasin2514 <yasinkhanrabbi1999@gmail.com>
2023-09-15 21:47:27 +0600

    add task form done

--- src/components/tasks/AddTaskModal.jsx
+++ src/components/tasks/AddTaskModal.jsx
@@ -1,19 +1,101 @@
+import { useForm } from "react-hook-form";
 import Modal from "../../ui/Modal";
 
 const AddTaskModal = ({ isOpen, setIsOpen }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+
+  //   handle reset
+  const handleReset = () => {
+    reset();
+    setIsOpen(false);
+  };
+  const onSubmit = (data) => {
+    console.log(data);
+    reset();
+    setIsOpen(false);
+  };
+
   return (
     <>
-      <Modal
-        isOpen={isOpen}
-        setIsOpen={setIsOpen}
-        title={"Component Modal Created"}
-      >
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
-          dolorem inventore assumenda molestias maiores incidunt cum magnam
-          beatae. Incidunt vero facere obcaecati harum quasi cum iusto in magni
-          iure quia.
-        </p>
+      <Modal isOpen={isOpen} setIsOpen={setIsOpen} title={"Add Your Task"}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="space-y-4">
+            <div className="flex flex-col gap-2 ">
+              <label htmlFor="title">Title</label>
+              <input
+                type="text"
+                id="title"
+                className="rounded-md"
+                placeholder="Enter your task title"
+                {...register("title")}
+              />
+            </div>
+            <div className="flex flex-col gap-2 ">
+              <label htmlFor="description">Description</label>
+              <textarea
+                type="text"
+                id="description"
+                className="rounded-md"
+                placeholder="Enter your description"
+                {...register("description")}
+              />
+            </div>
+            <div className="flex flex-col gap-2 ">
+              <label htmlFor="deadline">Deadline</label>
+              <input
+                type="date"
+                id="deadline"
+                className="rounded-md"
+                placeholder="Enter your task deadline"
+                {...register("deadline")}
+              />
+            </div>
+            <div className="flex flex-col gap-2 ">
+              <label htmlFor="assignTo">Assign To</label>
+              <select
+                id="assignTo"
+                className="rounded-md"
+                {...register("assignTo")}
+              >
+                <option value="Yasin Khan">Yasin Khan</option>
+                <option value="Rakib Mollah">Rakib Mollah</option>
+                <option value="Al Mamun">Al Mamun</option>
+                <option value="Majedul Hasan">Majedul Hasan</option>
+                <option value="Reduan Sahariar">Reduan Sahariar</option>
+                <option value="Anupom Paul">Anupom Paul</option>
+                <option value="Jisan">Jisan </option>
+                <option value="Miraz">Miraz</option>
+                <option value="Rakib Akter">Rakib Akter</option>
+              </select>
+            </div>
+            <div className="flex flex-col gap-2 ">
+              <label htmlFor="priority">Priority</label>
+              <select
+                id="priority"
+                className="rounded-md"
+                {...register("priority")}
+              >
+                <option value="Low">Low</option>
+                <option value="Medium">Medium</option>
+                <option value="High">High</option>
+              </select>
+            </div>
+          </div>
+          <div className="flex gap-4 justify-end mt-5">
+            <button type="reset" className="btn btn-danger">
+              {" "}
+              Reset
+            </button>
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+        </form>
       </Modal>
     </>
   );
--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -35,22 +35,12 @@ const Modal = ({ setIsOpen, isOpen,title,children }) => {
                 <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
                   <Dialog.Title
                     as="h3"
-                    className="text-lg font-medium leading-6 text-gray-900"
+                    className="text-lg font-medium text-center leading-6 text-gray-900"
                   >
                     {title}
                   </Dialog.Title>
                   <div className="mt-2">
                     {children}
-                  </div>
-
-                  <div className="mt-4">
-                    <button
-                      type="button"
-                      className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                      onClick={closeModal}
-                    >
-                      Got it, thanks!
-                    </button>
                   </div>
                 </Dialog.Panel>
               </Transition.Child>
